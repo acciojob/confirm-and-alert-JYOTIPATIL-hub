68a8743b91c100b570bf26b7a2b3f303939e188d
JYOTIPATIL-hub <142783664+JYOTIPATIL-hub@users.noreply.github.com>
2024-02-25 23:44:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// Ask for confirmation
+const userConfirmation = confirm("Do you want to proceed?");
+
+// Check user's choice
+if (userConfirmation) {
+  // User clicked OK
+  alert("You clicked OK. Proceeding...");
+} else {
+  // User clicked Cancel
+  alert("You clicked Cancel. Exiting...");
+}
